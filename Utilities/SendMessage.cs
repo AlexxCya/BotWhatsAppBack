1248da2dc2460f4f8ceabc6d812f21078c43d46b
Alex Jonguitud <ajungitud@itechdev.com.mx>
2022-09-13 15:16:56 -0500

    Prod Number

--- Utilities/SendMessage.cs
+++ Utilities/SendMessage.cs
@@ -11,14 +11,14 @@ namespace BotWhatsApp.Utilities
     {
         public static void SendMultimedia(string destinatario, string mensaje, string uri)
         {
-            TwilioClient.Init("AC03a47af37178d8017645a8cf9ccb9d07", "c361f34bb75523d8b1d2306dd6f0efbe"); //test
+            //TwilioClient.Init("AC03a47af37178d8017645a8cf9ccb9d07", "c361f34bb75523d8b1d2306dd6f0efbe"); //test
 
-            //TwilioClient.Init("AC2884ed3d73b7b474ec2567b55f69dffb", "e5879676508b7ba5c9b2cb5d0a1023c3");
+            TwilioClient.Init("AC2884ed3d73b7b474ec2567b55f69dffb", "e5879676508b7ba5c9b2cb5d0a1023c3");
 
             var messageOptions = new CreateMessageOptions(
                 new PhoneNumber("whatsapp:+" + destinatario));
-            messageOptions.From = new PhoneNumber("whatsapp:+14155238886");// ESTE SERA FIJO test
-            //messageOptions.From = new PhoneNumber("whatsapp:+5218141701660");// ESTE SERA FIJO
+            //messageOptions.From = new PhoneNumber("whatsapp:+14155238886");// ESTE SERA FIJO test
+            messageOptions.From = new PhoneNumber("whatsapp:+5218141701725");// ESTE SERA FIJO
             messageOptions.Body = mensaje;
             if (!string.IsNullOrEmpty(uri))
             {
